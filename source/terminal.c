4b6f7c2a012d86e1920efa7ddcfd92a69e2b0477
SegFault42 <rabougue@student.42.fr>
2017-05-12 01:10:44 +0200

    problem exec

--- source/terminal.c
+++ source/terminal.c
@@ -6,13 +6,14 @@
 /*   By: rabougue <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/11 21:23:03 by rabougue          #+#    #+#             */
-/*   Updated: 2017/05/11 21:52:13 by rabougue         ###   ########.fr       */
+/*   Updated: 2017/05/12 01:10:26 by rabougue         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/script.h"
 
 extern t_argp	g_argp[];
+char			*g_tab[] = {"llllllllllllll", NULL};
 
 void	init_tty(t_script *var_script)
 {
@@ -41,12 +42,26 @@ void	make_terminal_raw(t_script *var_script)
 
 void	create_shell(t_script *var_script, char **tab, char **environ)
 {
+	/*static char	*cmd[] = {"iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii", 0};*/
+
 	if (close(var_script->fd_ptmx) == -1)
 		critical_error("close ptmx failure\n");
 	if (ft_login_tty(var_script->fd_pts))
 		critical_error("login_tty failure\n");
-	if (execve(tab[0], &tab[0], environ) < 0)
-		write(2, "Execeve failure\n", 16);
+	if (var_script->cmd[0])
+	{
+		/*ft_memset(g_tab[1], 0, 255);*/
+		/*printf("%zu\n", sizeof(g_tab[0]));*/
+		printf("%s\n", g_tab[0]);
+		printf("%s\n", g_tab[1]);
+		if (execve(g_tab[0], &g_tab[0], environ) < 0)
+			write(2, "Execeve failure()\n", 16);
+	}
+	else
+	{
+		if (execve(tab[0], &tab[0], environ) < 0)
+			write(2, "Execeve failure\n", 16);
+	}
 	exit(EXIT_FAILURE);
 }
 
